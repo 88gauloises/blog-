f435d7e73a3fc61519eb4480a592aabf09d25ae4
間宮　勇志 <gauloises5296@outlook.jp>
2017-03-26 17:18:58 +0900

    contact add

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   resources :posts do
   resources :comments
 end
+  #get 'inquiry' => 'inquiry#index'
+  #post 'inquiry/confirm' => 'inquiry#confirm'
+  #post 'inquiry/thanks' => 'inquiry#thanks'
+
   root 'welcome#index'
   get "welcome/about"
+
+  get 'inquiry' => 'inquiry#index'
+  post 'inquiry/confirm'
+  post 'inquiry/thanks'
 end
